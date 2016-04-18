2cfc8c6bc4288c71fd773a1ee07d4c0c36831bc3
Jens Wilke <jw_github@headissue.com>
2016-04-18 14:35:02 +0200

    add new benchmarks which need eviction

--- jmh-run.sh
+++ jmh-run.sh
@@ -11,15 +11,21 @@
 # --quick   quick run with reduced benchmark time, to check for errors
 # --no3pty  only benchmark cache2k and JDK build ins, used for continuous benchmarking against performance regressions
 # --perf    run the benchmark with Linux perf (needs testing)
+# JAVA_HOME
 
 set -e;
 # set -x;
 
 test -n "$BENCHMARK_JVM_ARGS" || BENCHMARK_JVM_ARGS="-server -Xmx2G";
 
-test -n "$BENCHMARK_QUICK" || BENCHMARK_QUICK="-f 1 -wi 1 -i 1 -r 1s -foe true";
+# -wi warmup iterations
+# -w warmup time
+# -i number of iterations
+# -r time
+# -f how many time to fork a single benchmark
+test -n "$BENCHMARK_QUICK" || BENCHMARK_QUICK="-f 1 -wi 0 -i 1 -r 1s -foe true";
 
-test -n "$BENCHMARK_DILIGENT" || BENCHMARK_DILIGENT="-f 3 -wi 5 -i 5 -r 20s";
+test -n "$BENCHMARK_DILIGENT" || BENCHMARK_DILIGENT="-gc true -f 2 -wi 3 -w 5s -i 3 -r 30s";
 
 # Tinker benchmark options to do profiling and add assembler code output (linux only).
 # Needs additional disassembly library to display assembler code
@@ -28,20 +34,29 @@ test -n "$BENCHMARK_DILIGENT" || BENCHMARK_DILIGENT="-f 3 -wi 5 -i 5 -r 20s";
 # install with e.g.: mv ~/Downloads/linux-hsdis-amd64.so jdk1.8.0_45/jre/lib/amd64/hsdis-amd64.so.
 # For profiling only do one fork, but more measurement iterations
 # profilers are described here: http://java-performance.info/introduction-jmh-profilers
-test -z "$BENCHMARK_PERF" || BENCHMARK_PERF="-f 1 -wi 2 -i 5 -r 20s -prof perfasm";
+test -n "$BENCHMARK_PERFASM" || BENCHMARK_PERFASM="-f 1 -wi 2 -i 5 -r 30s -prof perfasm";
 
-STANDARD_PROFILER="-prof comp -prof gc -prof hs_rt -prof org.cache2k.benchmark.jmh.ForcedGcMemoryProfiler";
+STANDARD_PROFILER="-prof comp -prof gc -prof hs_rt";
+STANDARD_PROFILER="$STANDARD_PROFILER -prof org.cache2k.benchmark.jmh.ForcedGcMemoryProfiler";
+STANDARD_PROFILER="$STANDARD_PROFILER -prof org.cache2k.benchmark.jmh.MiscResultRecorderProfiler";
+STANDARD_PROFILER="$STANDARD_PROFILER -prof org.cache2k.benchmark.jmh.GcProfiler";
+
+# not used yet
+PERF_NORM_OPTIONS="-prof perfnorm:useDefaultStat=true"
 
 OPTIONS="$BENCHMARK_DILIGENT";
 
 unset no3pty;
+unset dry;
 
 processCommandLine() {
   while true; do
     case "$1" in
       --quick) OPTIONS="$BENCHMARK_QUICK";;
-      --perf) OPTIONS="$BENCHMARK_PERF";;
+      --perfasm) OPTIONS="$BENCHMARK_PERFASM";;
+      --perfnorm) OPTIONS="$BENCHMARK_PERFNORM";;
       --no3pty) no3pty=true;;
+      --dry) dry=true;;
       -*) echo "unknown option: $1"; exit 1;;
       *) break;;
     esac
@@ -64,7 +79,30 @@ local t=`date +%s`;
 echo "Total runtime $(( $t - $START_TIME ))s";
 }
 
+# quote argument if it is containing whitespace
+dryEcho() {
+  echo -n "java";
+  for i in "$@"; do
+    if [[ $i =~ [[:space:]] ]]; then
+      echo -n ' "'"$i"'"'
+    else
+      echo -n " $i";
+    fi
+  done
+  echo;
+}
+
 processCommandLine "$@";
+
+if test -z "$JAVA_HOME"; then
+  echo "JAVA_HOME needs to be set" 1>&2
+  exit 1;
+fi
+
+java=$JAVA_HOME/bin/java
+if test -n "$dry"; then
+  java="dryEcho";
+fi
 
 JAR="jmh-suite/target/benchmarks.jar";
 
@@ -81,7 +119,7 @@ TARGET="target/jmh-result";
 test -d $TARGET || mkdir -p $TARGET;
 
 if test -z "$no3pty"; then
-COMPLETE="$COMPLETE thirdparty.CaffeineCacheFactory thirdparty.GuavaCacheFactory";
+COMPLETE="$COMPLETE thirdparty.CaffeineCacheFactory thirdparty.GuavaCacheFactory thirdparty.EhCacheDirectFactory";
 fi
 
 startTimer;
@@ -97,11 +135,39 @@ for impl in $NO_EVICTION $COMPLETE; do
       fn="$TARGET/result-$runid";
       echo;
       echo "## $runid";
-      java -jar $JAR $benchmark -jvmArgs "$BENCHMARK_JVM_ARGS" $OPTIONS $STANDARD_PROFILER \
+      $java -jar $JAR $benchmark -jvmArgs "$BENCHMARK_JVM_ARGS" $OPTIONS $STANDARD_PROFILER \
            -t $threads -p cacheFactory=org.cache2k.benchmark.$impl \
            -rf json -rff "$fn.json" \
            2>&1 | tee $fn.out | filterProgress
-      echo "=> $fn.out";
+      if test -n "$dry"; then
+        cat $fn.out;
+      else
+        echo "=> $fn.out";
+      fi
+    done
+  done
+done
+
+#
+# Multi threaded with variable thread counts, with eviction
+#
+benchmarks="RandomSequenceCacheBenchmark NeverHitBenchmark";
+for impl in $COMPLETE; do
+  for benchmark in $benchmarks; do
+    for threads in 1 2 4; do
+      runid="$impl-$benchmark-$threads";
+      fn="$TARGET/result-$runid";
+      echo;
+      echo "## $runid";
+      $java -jar $JAR $benchmark -jvmArgs "$BENCHMARK_JVM_ARGS" $OPTIONS $STANDARD_PROFILER \
+           -t $threads -p cacheFactory=org.cache2k.benchmark.$impl \
+           -rf json -rff "$fn.json" \
+           2>&1 | tee $fn.out | filterProgress
+      if test -n "$dry"; then
+        cat $fn.out;
+      else
+        echo "=> $fn.out";
+      fi
     done
   done
 done
@@ -116,16 +182,24 @@ for impl in $NO_EVICTION $COMPLETE; do
     fn="$TARGET/result-$runid";
     echo;
     echo "## $runid";
-    java -jar $JAR $benchmark -jvmArgs "$BENCHMARK_JVM_ARGS" $OPTIONS $STANDARD_PROFILER \
+    $java -jar $JAR $benchmark -jvmArgs "$BENCHMARK_JVM_ARGS" $OPTIONS $STANDARD_PROFILER \
          -p cacheFactory=org.cache2k.benchmark.$impl \
          -rf json -rff "$fn.json" \
          2>&1 | tee $fn.out | filterProgress
+    if test -n "$dry"; then
+      cat $fn.out;
+    else
       echo "=> $fn.out";
+    fi
   done
 done
+
+if test -z "$dry"; then
 
 result=$TARGET/data.json
 # merge all results into single json file
 cat $TARGET/result-*.json | awk ' /^]/ { f=1; next; } f && /^\[/ { print "  ,"; next; } { print; } END { print "]"; }' > $result
+
+fi
 
 stopTimer;
---
+++ jmh-suite/src/main/java/org/cache2k/benchmark/jmh/suite/eviction/symmetrical/NeverHitBenchmark.java
@@ -0,0 +1,76 @@
+package org.cache2k.benchmark.jmh.suite.eviction.symmetrical;
+
+/*
+ * #%L
+ * Cache benchmark suite based on JMH.
+ * %%
+ * Copyright (C) 2013 - 2016 headissue GmbH, Munich
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import org.cache2k.benchmark.BenchmarkCache;
+import org.cache2k.benchmark.jmh.BenchmarkBase;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Prepopulate cache with 100k entries and access it in a random pattern
+ * with different hot rates.
+ *
+ * @author Jens Wilke
+ */
+@State(Scope.Benchmark)
+public class NeverHitBenchmark extends BenchmarkBase {
+
+  public static final int ENTRY_COUNT = 1000 * 1000;
+
+  private final static AtomicInteger offset = new AtomicInteger(0);
+
+  @State(Scope.Thread)
+  public static class ThreadState {
+    long index =  offset.getAndAdd(Integer.MAX_VALUE / 16);
+  }
+
+  BenchmarkCache<Integer, Integer> cache;
+
+  @Setup(Level.Iteration)
+  public void setup() throws Exception {
+    getsDestroyed = cache = getFactory().create(ENTRY_COUNT);
+  }
+
+  @Benchmark @BenchmarkMode(Mode.Throughput)
+  public long operation(ThreadState threadState, HitCountRecorder rec) {
+    int idx = (int) (threadState.index++);
+    Integer k = idx;
+    Integer v = cache.getIfPresent(k);
+    if (v != null) {
+      rec.hitCount++;
+    } else {
+      rec.missCount++;
+    }
+    cache.put(k, k);
+    return idx;
+  }
+
+}
