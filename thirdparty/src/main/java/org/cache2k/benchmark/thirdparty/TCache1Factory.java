70771ac9137b6853257d41090da0f259a9cda435
Jens Wilke <jw_github@headissue.com>
2016-08-30 11:01:53 +0200

    strip unused imports, reorganize copyrights

--- thirdparty/src/main/java/org/cache2k/benchmark/thirdparty/TCache1Factory.java
+++ thirdparty/src/main/java/org/cache2k/benchmark/thirdparty/TCache1Factory.java
@@ -1,20 +1,29 @@
-/*********************************************************************************
- * Copyright 2016-present trivago GmbH
- * 
+/*
+ * Additional copyright:
+ * Copyright 2016 trivago GmbH
+ */
+
+package org.cache2k.benchmark.thirdparty;
+
+/*
+ * #%L
+ * thirdparty
+ * %%
+ * Copyright (C) 2013 - 2016 headissue GmbH, Munich
+ * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- **********************************************************************************/
-
-package org.cache2k.benchmark.thirdparty;
+ * #L%
+ */
 
 import com.trivago.triava.tcache.TCacheFactory;
 import com.trivago.triava.tcache.core.Builder;
@@ -22,13 +31,7 @@ import com.trivago.triava.tcache.eviction.Cache;
 import org.cache2k.benchmark.BenchmarkCache;
 import org.cache2k.benchmark.BenchmarkCacheFactory;
 
-import java.util.Random;
-import java.util.UUID;
-import java.util.concurrent.Executor;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import static java.util.concurrent.TimeUnit.*;
 
 /**
  * Factory for TCache
@@ -91,7 +94,6 @@ public class TCache1Factory extends BenchmarkCacheFactory {
     @Override
     public void destroy() {
       factory.destroyCache(cache.id());
-//      cache.shutdown();
     }
 
     @Override
